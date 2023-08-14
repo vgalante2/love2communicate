6a05ec8095c9b4716bab6a6457a9f82d79851798
vgalante2 <vgalante0@gmail.com>
2023-08-14 15:51:13 -0400

    stars final

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,9 @@
 import React, { useRef } from "react";
 import Slider from "react-slick";
-import stars from "../assets/images/stars.png";
+
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import StarIcon from '@mui/icons-material/Star';
 
 const Testimonials = () => {
   
@@ -23,31 +24,33 @@ const Testimonials = () => {
     return (
       <div  className="testimonial-section">
         <h2 id="testimonials" className="review-header"> Hear What Our Parents Have to Say </h2>
+      
         <button className="prev-btn" onClick={() => slider?.current?.slickPrev()}> <ArrowBackIosIcon /> </button>
         <Slider className="review-slider" ref={slider} {...settings}>
-          <div className="review review-1">
-            <img src={stars} alt="stars" />
+          <div className="review ">
+            <h4> <StarIcon className="stars-icon" / > <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> </h4>
             <h3>Name </h3>
             <p> "From our very first session, the transformation in my child's speech and feeding skills was nothing short of remarkable. The therapist's gentle approach, paired with innovative techniques, made learning engaging and enjoyable." </p>
           </div>
-          <div className="review review-2">
-          <img src={stars} alt="stars" />
+          <div className="review">
+          <h4> <StarIcon className="stars-icon" / > <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> </h4>
             <h3>Name </h3>
             <p> ...Review</p>
           </div>
-          <div className="review review-3">
-          <img src={stars} alt="stars" />
+          <div className="review">
+          <h4> <StarIcon className="stars-icon" / > <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> </h4>
             <h3>Name </h3>
             <p> ...Review</p>
           </div>
-          <div className="review review-4">
-          <img src={stars} alt="stars" />
+          <div className="review">
+          <h4> <StarIcon className="stars-icon" / > <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> <StarIcon className="stars-icon" /> </h4>
             <h3>Name </h3>
             <p> ...Review</p>
           </div>
         </Slider>
         <br/>
         <button className="next-btn" onClick={() => slider?.current?.slickNext()}> <ArrowForwardIosIcon /> </button>
+      
       </div>
     );
   
