60df7a1fc5c6c378d363fc346ecb9d0a1d61cf00
vgalante2 <vgalante0@gmail.com>
2023-10-06 19:40:50 -0400

    About update

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -17,7 +17,7 @@ function AboutPage() {
         <div className="text-container">
             <p className="item-1"> Speech language pathologist M.A., CCC-SLP</p>
             <h1 className="item-2">Galina Kislin</h1>
-            <p className="item-3">Founder and owner with 23 years of experience as pediatric speech pathologist and a feeding  specialist. Worked in various settings, including early intervention, private practice, hospitals, daycare centers, preschools and elementary schools.
+            <p className="item-3"> CEO and Founder with 23 years of experience as pediatric speech pathologist and a feeding  specialist. Worked in various settings, including early intervention, private practice, hospitals, daycare centers, preschools and elementary schools.
  <br/> Passionate and dedicated to sharing her knowledge, empowering parents and other therapists through collaboration and education. Her philosophy is a whole child approach  from the initial evaluation until discharge.  A comprehensive speech and or feeding evaluation, collaboration with all providers and parents is a recipe for success in therapy.</p>
         </div>
     </div>
